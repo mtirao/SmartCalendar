2f1c333aa4191932450ecfe775f013d29b8c5057
Marcos Tirao <tirao@Marcoss-Mac-mini.local>
2014-09-11 19:40:13 -0300

    Initial Commit

---
+++ SmartCalendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartCalendar
+//
+//  Created by Marcos Tirao on 9/11/14.
+//  Copyright (c) 2014 Marcos Tirao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
